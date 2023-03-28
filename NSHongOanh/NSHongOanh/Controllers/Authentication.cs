dbf137235764582e2bb8b044bf822bb38fe202d5
NamNC <canhnamakashi@gmail.com>
2023-03-28 17:44:09 +0700

    Commit homepage

---
+++ NSHongOanh/NSHongOanh/Controllers/Authentication.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NSHongOanh.Controllers
+{
+    public class Authentication : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        public IActionResult PasswordChange()
+        {
+            return View();
+        }
+
+        public IActionResult PasswordReset()
+        {
+            return View();
+        }
+    }
+}
